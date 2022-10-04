790e023b7de5656b81edaa12ad06d8d322f722da
Furkan Acar <f.acar@azerion.com>
2022-10-04 20:53:02 +0300

    Added: react-hook-form, updated login and auth pages

---
+++ src/components/Login/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onSubmit", // can be onBlur as depend
+  });
+
+  function handleLoginFormSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(handleLoginFormSubmit)}>
+      <div className="mb-8">
+        <label
+          htmlFor="email"
+          className={`block font-bold text-sm mb-2 ${
+            errors?.email ? "text-red-400" : "text-purple-400"
+          }`}
+        >
+          Email
+        </label>
+        <input
+          autoComplete="off"
+          type="text"
+          name="email"
+          id="email"
+          placeholder="hey@chrisoncode.io"
+          className={`block w-full bg-transparent outline-none border-b-2 py-2 px-4  placeholder-purple-500 focus:bg-purple-600 ${
+            errors?.email
+              ? "text-red-300 border-red-400"
+              : "text-purple-200 border-purple-400"
+          }`}
+          {...register("email", {
+            required: "Your Email is required.",
+          })}
+        />
+        {errors?.email && (
+          <p className="text-red-500 text-sm mt-2">{errors?.email?.message}</p>
+        )}
+      </div>
+
+      <div className="mb-8">
+        <label
+          htmlFor="password"
+          className={`block font-bold text-sm mb-2 ${
+            errors?.password ? "text-red-400" : "text-purple-400"
+          }`}
+        >
+          Password
+        </label>
+        <input
+          autoComplete="off"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="superduperpassword"
+          className={`block w-full bg-transparent outline-none border-b-2 py-2 px-4 text-purple-200 focus:bg-purple-600 placeholder-purple-500 ${
+            errors?.password ? "border-red-400" : "border-purple-400"
+          }`}
+          {...register("password", {
+            required: "Your password is required.",
+          })}
+        />
+        {errors?.password && (
+          <p className="text-red-500 text-sm mt-2">
+            {errors?.password?.message}
+          </p>
+        )}
+      </div>
+
+      <button className="inline-block bg-purple-600 text-purple-200 rounded shadow py-2 px-5 text-sm">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Login;
--- src/containers/AuthPage/index.js
+++ src/containers/AuthPage/index.js
@@ -1,6 +1,40 @@
+import { useState } from "react";
+import Login from "../../components/Login";
+import Signup from "../../components/Signup";
+
 const AuthPage = () => {
+  const [hasAccount, setHasAccount] = useState(true);
+
+  const handleSetHasAccount = () => {
+    setHasAccount(!hasAccount);
+  };
   return (
-    <h1 className="text-3xl font-bold underline text-purple-500">Auth Page</h1>
+    <div className="px-10">
+      <h1 className="text-3xl flex justify-center font-bold underline text-purple-500">
+        Auth Page
+      </h1>
+      {hasAccount ? (
+        <div className="py-20 ">
+          <Login />
+        </div>
+      ) : (
+        <div className="py-20">
+          <Signup />
+        </div>
+      )}
+
+      {hasAccount ? (
+        <div className="flex flex-row">
+          <p>Are you new? Please join...</p>
+          <button onClick={handleSetHasAccount}>Register</button>
+        </div>
+      ) : (
+        <div className="flex  flex-row">
+          <p>Have you already have account? Please login...</p>
+          <button onClick={handleSetHasAccount}>Login</button>
+        </div>
+      )}
+    </div>
   );
 };
 
