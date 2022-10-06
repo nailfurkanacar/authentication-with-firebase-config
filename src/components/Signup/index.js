23e144e9343a662813c52875b08c86acadd88d7f
Furkan Acar <f.acar@azerion.com>
2022-10-06 21:31:28 +0300

    Added: signup page

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,7 +1,84 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
-  return <>Signup</>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onSubmit", // can be onBlur as depend
+  });
+
+  function handleSignupFormSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(handleSignupFormSubmit)}>
+      <div className="mb-8">
+        <label
+          htmlFor="email"
+          className={`block font-bold text-sm mb-2 ${
+            errors?.email ? "text-red-400" : "text-purple-400"
+          }`}
+        >
+          Email
+        </label>
+        <input
+          autoComplete="off"
+          type="text"
+          name="email"
+          id="email"
+          placeholder="sample@sample.com"
+          className={`block w-full bg-transparent outline-none border-b-2 py-2 px-4  placeholder-purple-500 focus:bg-purple-600 ${
+            errors?.email
+              ? "text-red-300 border-red-400"
+              : "text-purple-200 border-purple-400"
+          }`}
+          {...register("email", {
+            required: "Your Email is required.",
+          })}
+        />
+        {errors?.email && (
+          <p className="text-red-500 text-sm mt-2">{errors?.email?.message}</p>
+        )}
+      </div>
+
+      <div className="mb-8">
+        <label
+          htmlFor="password"
+          className={`block font-bold text-sm mb-2 ${
+            errors?.password ? "text-red-400" : "text-purple-400"
+          }`}
+        >
+          Password
+        </label>
+        <input
+          autoComplete="off"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="*******"
+          className={`block w-full bg-transparent outline-none border-b-2 py-2 px-4 text-purple-200 focus:bg-purple-600 placeholder-purple-500 ${
+            errors?.password ? "border-red-400" : "border-purple-400"
+          }`}
+          {...register("password", {
+            required: "Your password is required.",
+          })}
+        />
+        {errors?.password && (
+          <p className="text-red-500 text-sm mt-2">
+            {errors?.password?.message}
+          </p>
+        )}
+      </div>
+
+      <button className="inline-block bg-purple-600 text-purple-200 rounded shadow py-2 px-5 text-sm">
+        Register
+      </button>
+    </form>
+  );
 };
 
 export default Signup;
